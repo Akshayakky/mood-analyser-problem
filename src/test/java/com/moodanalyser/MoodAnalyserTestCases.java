020740b87fdb72bcd4f66cde2153da6851830170
akshayakky <akshaysmpt01@gmail.com>
2020-03-06 17:20:43 +0530

    ADDED : analyseMood Function To Take Message as Parameter And Return Mood

--- src/test/java/com/moodanalyser/MoodAnalyserTestCases.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTestCases.java
@@ -1,5 +1,13 @@
 package com.moodanalyser;
 
-public class MoodAnalyserTestCases {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyserTestCases {
+    @Test
+    public void name() {
+        MoodAnalyser moodanalyser = new MoodAnalyser();
+        String mood = moodanalyser.analyseMood("Sad");
+        Assert.assertEquals("SAD", mood);
+    }
 }
