fb7466c3bd404554345e5a211c8e0290a21a8e2e
akshayakky <akshaysmpt01@gmail.com>
2020-03-06 17:26:10 +0530

    ADDED : Test Case TC1.1

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -1,6 +1,10 @@
 package com.moodanalyser;
 
 public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Mood Analyser Problem");
+    }
+
     public String analyseMood(String message) {
         if (message.contains("Sad"))
             return "SAD";
--- src/test/java/com/moodanalyser/MoodAnalyserTestCases.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTestCases.java
@@ -5,9 +5,16 @@ import org.junit.Test;
 
 public class MoodAnalyserTestCases {
     @Test
-    public void name() {
+    public void givenMessage_WhenSad_ThenReturnSad() {
         MoodAnalyser moodanalyser = new MoodAnalyser();
         String mood = moodanalyser.analyseMood("Sad");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenContainsSad_ThenReturnSad() {
+        MoodAnalyser moodanalyser = new MoodAnalyser();
+        String mood = moodanalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("SAD", mood);
     }
 }
